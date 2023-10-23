363e816de00eb823e5dc761f6731ca9599493990
IguchiShunpei <k021g1129@g.neec.ac.jp>
2023-10-23 14:59:47 +0900

    画像ファイルをddsファイルに変換

--- TextureConverter.h
+++ TextureConverter.h
@@ -14,5 +14,21 @@ private:
 	void LoadWICTectureFromFile(const std::string& filepath);
 	//マルチバイト文字列をワイド文字列に変換
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mstring);
+	//フォルダパスとファイル名を分離する
+	void SeparatefilePath(const std::wstring& filePath);
+	//DDSテクスチャとしてファイル書き出し
+	void SaveDDSTextureToFIle();
+//メンバ変数
+private:
+	//画像の情報
+	DirectX::TexMetadata metadata_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+	//①ディレクトリパス
+	std::wstring directoryPath_;
+	//②ファイル名
+	std::wstring fileName_;
+	//③ファイル拡張子
+	std::wstring fileExt_;
 };
 
